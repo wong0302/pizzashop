db69d5a9b473ffdd556eb8186d15737104c77c4e
Aklito <akel0003@algonquinlive.com>
2019-04-12 06:33:57 -0400

    added order model schema

--- models/Order.js
+++ models/Order.js
@@ -1,1 +1,22 @@
-//TODO: Order schema
+//TODO: Recheck content
+const mongoose = require('mongoose')
+
+const orderSchema = new mongoose.Schema({
+    customr: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    type: {type: String, trim: true, lowercase: true, enum: ['pickup', 'delivery'], default: 'pickup'},
+    status: {type: String, trim: true, lowercase: true, enum: ['draft', 'ordered', 'paid', 'delivered'], default: 'draft'},
+    pizzas: [{type: mongoose.Schema.Types.ObjectId, ref: 'Pizzas'}],
+    //TODO: address required field must be set to true if type is delivery
+    address: {type: String, trim: true, lowercase: true, required: false},
+    price: {type: Number, default: 0},
+    //TODO: charge default field must be set to 500 if type is delivery
+    deliveryCharge: {type: Number, default: 0},
+    tax: {type: Number, default: 0},
+    total: {type: Number, default: 0},
+},
+{
+    timestamps: true
+})
+
+const Model = mongoose.model('Order', orderSchema)
+module.exports = Model
